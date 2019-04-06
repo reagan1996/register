8a5ee54ce7d2e57d2c72a0d73f0e70978e678456
Reagan Prince <RPrince@spartaglobal.com>
2019-04-06 19:15:31 +0100

    added run_register method to Cohort class

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 from cohort import Cohort
 
-def register():
-    course = Cohort("Data1")
-    course.get_student_data("students.txt")
-    course.add_raw_student_data()
-    print("Hello, time for the register?")
-    total_num_of_students = len(course.students)
-    num_of_absent_students = 0
-    for student in course.students:
-        no_valid_answer = True
-        while no_valid_answer:
-            present = input("\nIs " + student.student_name() + " here?(y/n) ")
-            if present.lower() == "y":
-                no_valid_answer = False
-            elif present.lower() == "n":
-                num_of_absent_students += 1
-                no_valid_answer = False
-            else:
-                print("\nInvalid option entered. Please try again")
-
-    print("\nToday we have " + str(
-        int((100 * (total_num_of_students - num_of_absent_students)) / total_num_of_students)) + "% attendence, with " +
-          str(num_of_absent_students) + " absent.")
-
-
-register()
-
+course = Cohort("Data1")
+course.get_student_data("students.txt")
+course.add_raw_student_data()
+course.run_register()
 
 
 
